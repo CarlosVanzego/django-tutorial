4e29efb5b942e47ee2245a8941ce994f207eaaed
Your Name <carlosvanzego96@gmail.com>
2025-05-03 09:36:11 -0500

    wsgi.py comments commit

--- djangotutorial/mysite/wsgi.py
+++ djangotutorial/mysite/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
+# this import is used to set the default value for the DJANGO_SETTINGS_MODULE environment variable, similar to the asgi.py file
 import os
-
+# this line is importing the get_wsgi_application function from the django.core.wsgi module
 from django.core.wsgi import get_wsgi_application
-
+# os.environ.setdefault is used to set the default value for the DJANGO_SETTINGS_MODUEL environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
+# application is set to the result of calling get_wsgi_application
 application = get_wsgi_application()
