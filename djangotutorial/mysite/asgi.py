857148614c59167378d10188939581353e6deb6b
Your Name <carlosvanzego96@gmail.com>
2025-05-03 09:32:38 -0500

    asgi.py comments commit

--- djangotutorial/mysite/asgi.py
+++ djangotutorial/mysite/asgi.py
@@ -6,11 +6,13 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-
+# this is a configuration file for the Django ASGO server
+# it is importing the os module to set the environment variable
 import os
-
+# this line is importing the get_asgi_application function from the django.core.asgi module
 from django.core.asgi import get_asgi_application
-
+# os.environ.setdefault is used to set the default value for the DJANGO_SETTINGS_MODULE environment variable
+# mysite.settings is the settings module for the Django project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
+# the applicaiton variable is set to the result of calling get_asgi_application
 application = get_asgi_application()
